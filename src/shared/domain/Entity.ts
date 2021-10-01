ea07c3b16804ab605dda4a8b0cd973ba9361cd44
Davi Almeida <davistrife@live.com>
2021-10-01 17:38:38 -0300

    feat: create entity base

---
+++ src/shared/domain/Entity.ts
@@ -0,0 +1,11 @@
+import { v4 } from "uuid"
+
+export abstract class Entity<T> {
+    protected readonly uuid: string
+    protected props: T
+  
+    constructor(props: T, uuid?: string) {
+      this.uuid = uuid ? uuid : v4()
+      this.props = props
+    }
+  }
