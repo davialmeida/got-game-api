203d044fcbc23f51d6a7327e6c2551e71255b55c
Davi Almeida <davistrife@live.com>
2021-10-01 17:39:29 -0300

    feat: add ihouse props

---
+++ src/modules/houses/interfaces/IHouseProps.ts
@@ -0,0 +1,9 @@
+import { Lord } from "../../lords/Lord";
+
+export interface IHouseProps {
+    uuid?: string;
+    name: string;
+    region: string;
+    foundation_year?: string;
+    current_lord?: Lord | null;
+}
