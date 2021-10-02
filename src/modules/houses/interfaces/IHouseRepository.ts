40f7cb7547770f67cf78d03c3067a01676569486
Davi Almeida <davistrife@live.com>
2021-10-02 16:12:36 -0300

    refactor: create house interface

--- src/modules/houses/interfaces/IHouseRepository.ts
+++ src/modules/houses/interfaces/IHouseRepository.ts
@@ -1,7 +1,8 @@
+import { Lord } from "src/modules/lords/Lord";
 import { House } from "../House"
 
 export interface IHouseRepository {
-    create: (house: House) => Promise<House | null>
+    create: (house: House, lord: Lord | null) => Promise<House | null>
     list: () => Promise<Array<House> | null>
     findByName: (name: string) => Promise<Array<House> | null>
     findByID: (uuid: string) => Promise<House | null>
