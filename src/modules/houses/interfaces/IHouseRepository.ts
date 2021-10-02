121fe219c66b6a5153ca9ba39cb9c6cbb8845549
Davi Almeida <davistrife@live.com>
2021-10-01 23:27:25 -0300

    feat: add delete by id property

--- src/modules/houses/interfaces/IHouseRepository.ts
+++ src/modules/houses/interfaces/IHouseRepository.ts
@@ -1,9 +1,13 @@
 import { House } from "../House";
 
+export interface DeletedItem {
+    message: string
+}
+
 export interface IHouseRepository {
     create: (house: House) => Promise<House | null>
     list: () => Promise<Array<House> | null>
     findByName: (name: string) => Promise<Array<House> | null>
     findByID: (uuid: string) => Promise<House | null>
-    /*deleteId: () => Promise<>*/
+    deleteById: (uuid: string) => Promise<null>
 }
