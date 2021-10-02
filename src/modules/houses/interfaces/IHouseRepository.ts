10c5cadf8382909f6a08fbb7115722e5696c3490
Davi Almeida <davistrife@live.com>
2021-10-01 23:11:28 -0300

    feat: add interface findByName

--- src/modules/houses/interfaces/IHouseRepository.ts
+++ src/modules/houses/interfaces/IHouseRepository.ts
@@ -3,7 +3,7 @@ import { House } from "../House";
 export interface IHouseRepository {
     create: (house: House) => Promise<House | null>
     list: () => Promise<Array<House> | null>
-    /*findByName: () => Promise<>
-    findByID: () => Promise<>
+    findByName: (name: string) => Promise<Array<House> | null>
+    /*findByID: () => Promise<>
     deleteId: () => Promise<>*/
 }
